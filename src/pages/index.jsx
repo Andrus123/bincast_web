3c0ebd316be557354141bbb3e12a9bb197f0fc1a
Andrés K. Aquin <andruuus123@gmail.com>
2021-07-30 22:59:53 -0400

    Styling

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,26 @@
 // import React and routing dependencies
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// import shared layout component
+import Layout from "../components/Layout";
 // import routes
-import Home from './home';
-import MyNotes from './mynotes';
-import Favorites from './favorites';
+import Home from "./home";
+import MyNotes from "./mynotes";
+import Favorites from "./favorites";
 
 // define routes
 const Pages = () => {
-    return (
-        <Router>
-            <Route exact path="/" component={Home} />
-            <Route path="/mynotes" component={MyNotes} />
-            <Route path="/favorites" component={Favorites} />
-        </Router>
-    );
+  return (
+    <Router>
+      {/* Wrap our routes within the Layout component */}
+      <Layout>
+        <Route exact path="/" component={Home} />
+        <Route path="/mynotes" component={MyNotes} />
+        <Route path="/favorites" component={Favorites} />
+      </Layout>
+    </Router>
+  );
 };
 
 export default Pages;
