7a91900372b979a2332cc326defc4e1bd1ec3405
Andrés K. Aquin <andruuus123@gmail.com>
2021-07-30 20:22:42 -0400

    Linking

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+//import the Link component fom react-router
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>Bincast</h1>
       <p>This is the home page</p>
+      {/* add a list of links*/}
+      <ul>
+        <li>
+          <Link to="/mynotes">My Bins</Link>
+        </li>
+        <li>
+          <Link to="/favorites">Favorites</Link>
+        </li>
+      </ul>
     </div>
   );
 };
