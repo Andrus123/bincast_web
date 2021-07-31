92413467ee132e7410f0df3a507c4de30c831b32
Andrés K. Aquin <andruuus123@gmail.com>
2021-07-30 20:40:12 -0400

    UI Components

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 //import the Link component fom react-router
-import { Link } from 'react-router-dom';
+import Header from '../components/Header';
+import Navigation from "../components/Navigation";
 
 const Home = () => {
   return (
     <div>
-      <h1>Bincast</h1>
+      <Header />
+      <Navigation />
       <p>This is the home page</p>
-      {/* add a list of links*/}
-      <ul>
-        <li>
-          <Link to="/mynotes">My Bins</Link>
-        </li>
-        <li>
-          <Link to="/favorites">Favorites</Link>
-        </li>
-      </ul>
     </div>
   );
 };
